c48b0ef43bf2b830275f21aa1fc979d5c30c6d47
William Ghelfi <trumbitta@gmail.com>
2019-09-08 22:52:49 +0200

    refactor: tweak footer

--- src/components/ui/ui-footer/ui-footer.component.tsx
+++ src/components/ui/ui-footer/ui-footer.component.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-
 import styled from 'styled-components';
 
-import { colorRoles, components, fonts } from '../../styled/_variables';
+import { colorRoles, components, fonts, rhythm } from '../../styled/_variables';
 import { uiComponentsBox } from '../../styled/mixins/_components';
 
 export const UiFooter = styled.footer`
   ${uiComponentsBox}
+  padding-top: ${rhythm[4]};
+  padding-bottom: ${rhythm[4]};
   background-color: ${colorRoles.light};
   border: ${components.border.thickness} solid ${components.border.color};
   font-family: ${fonts.families.sansSerif};
