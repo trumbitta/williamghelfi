ea0383a783397679c5dcba237a717ef8792f94d1
William Ghelfi <trumbitta@gmail.com>
2022-05-27 12:46:31 +0000

    ci: enable build on Netlify

---
+++ apps/site/pages/bootstrap-in-practice/index.tsx
@@ -0,0 +1,16 @@
+export const BootstrapInPractice = () => (
+  <>
+    <p>
+      The book was for the earliest releases of Bootstrap 3.
+      <br />
+      Bootstrap is now at version 4, heading for 5.
+    </p>
+    <p>That&#39;s why the book is now free. Enjoy!</p>
+    <p>
+      <a href="/bootstrap-in-practice/Bootstrap_In_Practice-Complete.zip">
+        Download for free [PDF &middot; 30MB]
+      </a>
+    </p>
+  </>
+);
+export default BootstrapInPractice;
